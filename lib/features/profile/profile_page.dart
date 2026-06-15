152ed6c7a0480f185f7f9a4ab925eea1c1057cc4
msayed235753 <m.sayed235753@gmail.com>
2026-06-16 01:47:14 +0300

    updated version

--- lib/features/profile/profile_page.dart
+++ lib/features/profile/profile_page.dart
@@ -16,7 +16,7 @@ TextStyle _f({double sz = 14, FontWeight fw = FontWeight.w400, Color? c, double?
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
-  static const String _appVersion = '1.0.1';
+  static const String _appVersion = '1.0.6';
 
   @override
   Widget build(BuildContext context) {
